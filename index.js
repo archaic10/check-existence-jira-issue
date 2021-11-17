5c67e760039ee9a3f993482497135f3c8178e2e5
archaic10 <vitor.franca.9400@gmail.com>
2021-11-17 13:18:02 -0300

    feat: creation of action to Check the existence of an issue in the jira

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const axios = require('axios')
+
+async function run(){
+    try{
+        let url = core.getInput('url')
+        let basic_auth = core.getInput('basic_auth')
+        await verifyJiraIssue(url, basic_auth)
+    }catch(error){
+        core.setFailed(error.message)
+    }    
+}
+run()
+
+async function verifyJiraIssue(url, basic_auth){
+    await axios.get(url,
+        {
+            headers: {
+              Authorization: basic_auth,
+            }
+    
+    }).then(res => {
+        core.setOutput("result", "The issue was found successfully!")
+    }).catch(err => {
+        core.setFailed("The Issue not found!")
+    })
+}
